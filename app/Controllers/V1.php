d38470fdfaceccbcfe8141f5d73f311c0150139a
Alan <msinfo45@gmail.com>
2020-10-23 11:33:35 +0700

    Penambahan fungsi pencarian by size di produk

---
+++ app/Controllers/V1.php
@@ -0,0 +1,31 @@
+<?php namespace App\Controllers;
+
+class v1 extends BaseController
+{
+	public function index()
+	{
+		return view('products/index');
+
+	}
+
+	public function load_products()
+	{
+		return view('products/view/load_products');
+		
+	}
+
+	public function products()
+
+	{
+
+
+	return view('v1/products');
+
+		//return view('products/view/load_products');
+		
+	}
+
+
+	//--------------------------------------------------------------------
+
+}
